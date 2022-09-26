54c0fef85c8e7ab5020d0ac66c8d438ab6d6decf
Mario Rios <mariorios007@hotmail.com>
2022-09-26 17:11:10 -0500

    git commit

---
+++ Bootcamp.Api/Controllers/DocumenTypeController.cs
@@ -0,0 +1,26 @@
+﻿using Bootcamp.Queries.DocumentType;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bootcamp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumenTypeController : ControllerBase
+    {
+        private readonly IDocumenTypeQueries _documenTypeQueries;
+
+        public DocumenTypeController(IDocumenTypeQueries documenTypeQueries)
+        {
+            _documenTypeQueries = documenTypeQueries;
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public async Task<ActionResult> GetAll()
+        {
+            var result = await _documenTypeQueries.GetAll();
+            return Ok(result);
+        }
+    }
+}
---
+++ Bootcamp.Queries/DocumentType/DocumenTypeQueries.cs
@@ -0,0 +1,32 @@
+﻿using Bootcamp.ViewModel;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bootcamp.Queries.DocumentType
+{
+    public class DocumenTypeQueries : IDocumenTypeQueries
+    {
+        private readonly string _connectionString;
+        public DocumenTypeQueries(IConfiguration configuration)
+        {
+            _connectionString = configuration["ConnectionStrings:SQL"];
+        }
+        public async Task<IEnumerable<DocumenTypeViewModel>> GetAll()
+        {
+            IEnumerable<DocumenTypeViewModel> result = new List<DocumenTypeViewModel>();
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                result = await connection.QueryAsync<DocumenTypeViewModel>("[dbo].[Usp_Sel_DocumentType]", commandType: System.Data.CommandType.StoredProcedure);
+            }
+
+            return result;
+        }
+    }
+}
